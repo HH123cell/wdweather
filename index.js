becb9fecc7397c8c716bae6926151a519f0c0a7e
HH123cell <1101374304@qq.com>
2020-03-11 21:16:24 +0800

    初次提交

---
+++ index.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    var animal = ['白兔', '蜻蜓'];
+  
+    animal.forEach(v => {
+      console.log('v ==> ', v);
+    })
+  
+    console.log('loaded');
+    function fn2(){
+        var b = 2;
+        return b;
+    }
+  
+  }
